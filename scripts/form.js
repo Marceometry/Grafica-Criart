b5d9a26fb9a777c5bcf27a2162825acefdd2afea
Marcelino <marceometry@gmail.com>
2021-04-09 14:46:12 -0300

    js validations

--- scripts/form.js
+++ scripts/form.js
@@ -1,18 +1,44 @@
-function validateForm() {
+function validateForm(form) {
+    const cpf = document.getElementById('cpf')
+    const nome = document.getElementById('nome')
+    const email = document.getElementById('email')
+
     const masc = document.getElementById('masc')
     const fem = document.getElementById('fem')
     const outro = document.getElementById('outro')
 
-    if (masc.checked == false && fem.checked == false && outro.checked == false) {
+    const motivo = document.getElementById('motivo')
+
+    if (cpf.value == '' || nome.value == '' || email.value == '' || motivo.value == '') {
+        alert('Você deve preencher os campos obrigatórios.')
+        return
+    } else if (masc.checked == false && fem.checked == false && outro.checked == false) {
         alert('Por favor, escolha uma das opções de sexo.')
+        return
+    } else {
+        form.submit()
+        alert('Agradecemos pela comunicação!')
+        location.replace('index.html')
     }
 }
 
-function validaSenha() {
+function validateLoginForm(form) {
+    const cpf = document.getElementById('cpf')
+    const nome = document.getElementById('nome')
+    const email = document.getElementById('email')
+
     senha = document.getElementById('senha')
     senha2 = document.getElementById('senha2')
 
-    if (senha.value != senha2.value) {
-        alert('As duas senhas não batem.')
+    if (cpf.value == '' || nome.value == '' || email.value == '') {
+        alert('Você deve preencher todos os campos.')
+        return
+    } else if (senha.value != senha2.value) {
+        alert('As duas senhas devem ser iguais.')
+        return
+    } else {
+        form.submit()
+        alert('Login realizado!')
+        location.replace('index.html')
     }
 }
