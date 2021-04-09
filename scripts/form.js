221a6c65988919e4c41e5679869f00bd438173c6
Marcelino <marceometry@gmail.com>
2021-04-09 12:46:27 -0300

    form validation

---
+++ scripts/form.js
@@ -0,0 +1,9 @@
+function validateForm(form) {
+    const masc = document.getElementById('masc')
+    const fem = document.getElementById('fem')
+    const outro = document.getElementById('outro')
+
+    if (masc.checked == false && fem.checked == false && outro.checked == false) {
+        alert('Por favor, escolha uma das opções de sexo.')
+    }
+}
