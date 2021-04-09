f2d31b67e6b975013db369b28ae13a49eca63fcd
Marcelino <marceometry@gmail.com>
2021-04-09 12:50:02 -0300

    form validation

--- scripts/form.js
+++ scripts/form.js
@@ -8,4 +8,11 @@ function validateForm() {
     }
 }
 
-function validaSenha() {}
+function validaSenha() {
+    senha = document.getElementById('senha')
+    senha2 = document.getElementById('senha2')
+
+    if (senha.value != senha2.value) {
+        alert('As duas senhas não batem.')
+    }
+}
